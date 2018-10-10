4bf4d1c1317c67f30d1c7c1dc2e05084a4a69c65
rbtprograms <rbt61690@gmail.com>
2018-10-10 07:08:48 -0700

    loading data working

--- src/components/blog/reducers.test.js
+++ src/components/blog/reducers.test.js
@@ -2,12 +2,23 @@ import {
   // selectedPhoto,
   essays,
   // PHOTO_SELECTED,
-  // ESSAYS_LOADED
+  ESSAYS_LOADED
 } from './reducers';
 
 describe('essays reducer', () => {
   it('initializes to an empty array', () => {
     const state = essays(undefined, {});
     expect(state).toEqual([]);
+  });
+
+  it('loads data', () => {
+    const payload = [{}, {}, {}];
+
+    const state = essays([], {
+      type: ESSAYS_LOADED,
+      payload
+    });
+
+    expect(state).toBe(payload);
   });
 });
