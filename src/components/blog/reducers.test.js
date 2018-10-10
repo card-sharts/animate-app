959fb23f06f593a4178a7c2e020333384728ec8a
rbtprograms <rbt61690@gmail.com>
2018-10-10 07:13:52 -0700

    reducers tests for essays done for now

--- src/components/blog/reducers.test.js
+++ src/components/blog/reducers.test.js
@@ -1,9 +1,28 @@
 import { 
-  // selectedPhoto,
+  selectedPhoto,
   essays,
-  // PHOTO_SELECTED,
+  PHOTO_SELECTED,
   ESSAYS_LOADED
 } from './reducers';
+
+describe('selectedPhoto reducer', () => {
+  it('initializes to null', () => {
+    const state = selectedPhoto(undefined, {});
+    expect(state).toBe(null);
+  });
+
+  it('loads data', () => {
+    const payload = { data: 1 };
+
+    const state = selectedPhoto(null, {
+      type: PHOTO_SELECTED,
+      payload
+    });
+
+    expect(state).toBe(payload);
+    expect(state.data).toBe(1);
+  });
+});
 
 describe('essays reducer', () => {
   it('initializes to an empty array', () => {
@@ -12,13 +31,15 @@ describe('essays reducer', () => {
   });
 
   it('loads data', () => {
-    const payload = [{}, {}, {}];
+    const payload = [{ data: 1 }, { data: 2 }, { data: 3 }];
 
     const state = essays([], {
       type: ESSAYS_LOADED,
       payload
     });
 
-    expect(state).toBe(payload);
+    expect(state[0].data).toBe(1);
+    expect(state[1].data).toBe(2);
+    expect(state[2].data).toBe(3);
   });
 });
