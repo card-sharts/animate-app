19070eda93506397a307039eb69fff16423804ba
Arthur Jen <arthurljen@gmail.com>
2018-11-05 17:43:08 -0800

    photos show up on essay view

--- src/components/blog/Essay.js
+++ src/components/blog/Essay.js
@@ -1,19 +1,14 @@
 import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Photos from './Photos';
-import ReactModal from 'react-modal';
 import { connect } from 'react-redux';
-import { getSelectedPhoto, getSelectedEssay } from './reducers';
+import { getSelectedEssay } from './reducers';
 import { loadEssay } from './actions';
 
 class Essay extends PureComponent {
-  state = {
-    showModal: false,
-    essay: null
-  };
 
   static propTypes = {
-    selectedPhoto: PropTypes.object,
+    essay: PropTypes.object,
     match: PropTypes.object,
     loadEssay: PropTypes.func.isRequired
   };
@@ -23,41 +18,24 @@ class Essay extends PureComponent {
     loadEssay(match.params.id);
   }
 
-  handleToggleModal = () => {
-    let { showModal } = this.state;
-    this.setState({ showModal: !showModal });
-  };
-
   render() { 
-    const { title, photos } = this.state;
-    const { selectedPhoto } = this.props;
+    const { essay } = this.props;
 
     return (
-      <section>
-        <h2>{title}</h2>
-        <Photos photos={photos} toggleModal={this.handleToggleModal}/>
-        <ReactModal
-          isOpen={this.state.showModal}
-          contentLabel='modalTest'
-          ariaHideApp={false}
-        >
-          {selectedPhoto && 
-            <Fragment>
-              <button onClick={this.handleToggleModal}>Close</button>
-              <h2>{selectedPhoto.caption}</h2>
-              <h2>{selectedPhoto._id}</h2>
-              <img src={selectedPhoto.url}/>
-            </Fragment>
-          }
-        </ReactModal>
-      </section>
+      <Fragment>
+        {essay && 
+          <section>
+            <h2>{essay.title}</h2>
+            <Photos photos={essay.photos}/>
+          </section>
+        }
+      </Fragment>
     );
   }
 }
  
 export default connect(
   (state) => ({
-    selectedPhoto: getSelectedPhoto(state),
     essay: getSelectedEssay(state)
   }),
   { loadEssay }
--- src/components/blog/Photo.js
+++ src/components/blog/Photo.js
@@ -1,35 +1,21 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import styles from './Photo.css';
-import { connect } from 'react-redux';
-import { setModalPhoto } from './actions';
 
 class Photo extends PureComponent {
   static propTypes = {
     photo: PropTypes.object.isRequired,
-    toggleModal: PropTypes.func.isRequired,
-    setModalPhoto: PropTypes.func.isRequired
-  };
-
-  handleModal = () => {
-    const { toggleModal, photo, setModalPhoto } = this.props;
-    setModalPhoto(photo);
-    toggleModal();
   };
 
   render() { 
-    const { url, caption, tags  } = this.props.photo;
+    const { photoUrl, caption, tags  } = this.props.photo;
 
     return (
-      <div className={styles.photo} onClick={this.handleModal}>
-        <img src={url}/>
+      <div className={styles.photo}>
+        <img src={photoUrl}/>
       </div>
     );
   }
 }
  
-export default connect(
-  null,
-  { setModalPhoto }
-)(Photo);
+export default Photo;
